d2ebe4c46fc989f95e970ebe4824c8d4d5e594d5
Clara Belitz <clara.belitz@tamr.com>
2020-03-25 15:19:05 -0600

    changes to make the module actually work (some syntax in policies, adding key to instance), updates to READMEs, and main.tf as example to use
    making main.tf generic, fixing readmes to point to github

--- modules/aws-iam-policies/variables.tf
+++ modules/aws-iam-policies/variables.tf
@@ -1,9 +1,27 @@
 variable "aws_role_name" {
-    type = string
-    description = "IAM Role to which the policy will be attached"
+  type = string
+  description = "IAM Role to which the policy will be attached"
 }
 
 variable "aws_account_id" {
-    type = string
-    description = "AWS account in which the cluster will be created"
+  type = string
+  description = "AWS account in which the cluster will be created"
+}
+
+variable "aws_emrfs_hbase_bucket_name" {
+  type = string
+  description = "AWS account in which the cluster will be created"
+  default = ""
+}
+
+variable "aws_emrfs_hbase_logs_bucket_name" {
+  type = string
+  description = "AWS account in which the cluster will be created"
+  default = ""
+}
+
+variable "aws_emrfs_spark_logs_bucket_name" {
+  type = string
+  description = "AWS account in which the cluster will be created"
+  default = ""
 }
