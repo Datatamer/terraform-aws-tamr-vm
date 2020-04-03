7ffdb6e7aa3c09f3b11204379474783d0b71987d
Clara Belitz <clara.belitz@tamr.com>
2020-04-03 17:13:40 -0600

    bump version, fix a variable that got dropped in rebasing

--- modules/aws-iam-policies/variables.tf
+++ modules/aws-iam-policies/variables.tf
@@ -1,3 +1,15 @@
+variable "aws_emr_creator_policy_name" {
+  type = string
+  description = "The name to give to the policy regarding EMR permissions"
+  default = "emrCreatorMinimalPolicy"
+}
+
+variable "aws_emrfs_user_policy_name" {
+  type = string
+  description = "The name to give to the policy regarding S3 permissions"
+  default = "emrfsUserMinimalPolicy"
+}
+
 variable "aws_role_name" {
   type = string
   description = "IAM Role to which the policy will be attached"
@@ -11,22 +23,14 @@ variable "aws_account_id" {
 variable "aws_emrfs_hbase_bucket_name" {
   type = string
   description = "AWS account in which the cluster will be created"
-  default = ""
 }
 
 variable "aws_emrfs_hbase_logs_bucket_name" {
   type = string
   description = "AWS account in which the cluster will be created"
-}
-
-variable "aws_emrfs_hbase_logs_bucket_name" {
-  type = string
-  description = "AWS account in which the cluster will be created"
-  default = ""
 }
 
 variable "aws_emrfs_spark_logs_bucket_name" {
   type = string
   description = "AWS account in which the cluster will be created"
-  default = ""
 }
