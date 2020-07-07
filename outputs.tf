6a9d74bd41770702ebc8c382647aaf27fd56f42c
Rosemary Wang <66625360+rosemarywang@users.noreply.github.com>
2020-07-07 10:23:48 -0400

    CI-159 Update Makefile

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "tamr_security_group_id" {
-  value = module.aws-security-groups.tamr_security_group_id
+  value       = module.aws-security-groups.tamr_security_group_id
   description = "ID of the security group created"
 }
 
 output "tamr_instance_ip" {
-  value = module.tamr_instance.tamr_instance_ip
+  value       = module.tamr_instance.tamr_instance_ip
   description = "Private IP address of the Tamr instance"
 }
--- variables.tf
+++ variables.tf
@@ -1,193 +1,193 @@
 variable "aws_emr_creator_policy_name" {
-  type = string
+  type        = string
   description = "The name to give to the policy regarding EMR permissions"
-  default = "emrCreatorMinimalPolicy"
+  default     = "emrCreatorMinimalPolicy"
 }
 
 variable "aws_emrfs_user_policy_name" {
-  type = string
+  type        = string
   description = "The name to give to the policy regarding S3 permissions"
-  default = "emrfsUserMinimalPolicy"
+  default     = "emrfsUserMinimalPolicy"
 }
 
 variable "aws_role_name" {
-  type = string
+  type        = string
   description = "IAM Role to create, and to which the policies will be attached"
 }
 
 variable "aws_instance_profile_name" {
-  type = string
+  type        = string
   description = "IAM Instance Profile to create"
 }
 
 variable "aws_account_id" {
-  type = string
+  type        = string
   description = "AWS account in which the cluster will be created"
 }
 
 variable "aws_emrfs_hbase_bucket_name" {
-  type = string
+  type        = string
   description = "AWS account in which the cluster will be created"
 }
 
 variable "aws_emrfs_hbase_logs_bucket_name" {
-  type = string
+  type        = string
   description = "AWS account in which the cluster will be created"
 }
 
 variable "aws_emrfs_spark_logs_bucket_name" {
-  type = string
+  type        = string
   description = "AWS account in which the cluster will be created"
 }
 
 variable "vpc_id" {
-  type = string
+  type        = string
   description = "The ID of the VPC in which to attach the security group"
 }
 
 variable "ami" {
-  type = string
+  type        = string
   description = "The AMI to use for the EC2 instance"
 }
 
 variable "availability_zone" {
-  type = string
+  type        = string
   description = "The availability zone to use for the EC2 instance"
-  default = "us-east-1a"
+  default     = "us-east-1a"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "The instance type to use for the EC2 instance"
-  default = "c5.9xlarge"
+  default     = "c5.9xlarge"
 }
 
 variable "volume_type" {
-  type = string
+  type        = string
   description = "The type of root block volume to attach to the EC2 instance"
-  default = "gp2"
+  default     = "gp2"
 }
 
 variable "volume_size" {
-  type = number
+  type        = number
   description = "The size of the root block volume to attach to the EC2 instance"
-  default =  100
+  default     = 100
 }
 
 variable "tamr_instance_tags" {
-  type = map(string)
+  type        = map(string)
   description = "Additional tags to be attached to the Tamr EC2 instance"
-  default = {Author: "Tamr", Name: "Tamr VM"}
+  default     = { Author : "Tamr", Name : "Tamr VM" }
 }
 
 variable "key_name" {
-  type = string
+  type        = string
   description = "The key name to attach to the EC2 instance for SSH access"
 }
 
 variable "sg_name" {
-  type = string
+  type        = string
   description = "Security Group to create"
-  default = "tamr-instance-security-group"
+  default     = "tamr-instance-security-group"
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
   description = "The subnet to create the EC2 instance in"
 }
 
 variable "tamr_ui_port" {
-  type = number
+  type        = number
   description = "Port for Tamr UI and proxying Tamr services"
-  default = 9100
+  default     = 9100
 }
 
 variable "tamr_es_port" {
-  type = number
+  type        = number
   description = "Port for Tamr elasticsearch"
-  default = 9200
+  default     = 9200
 }
 
 variable "tamr_auth_port" {
-  type = number
+  type        = number
   description = "Port for Tamr auth"
-  default = 9020
+  default     = 9020
 }
 
 variable "tamr_persistence_port" {
-  type = number
+  type        = number
   description = "Port for Tamr persistence"
-  default = 9080
+  default     = 9080
 }
 
 variable "zk_port" {
-  type = number
+  type        = number
   description = "Port for accessing Zookeeper on the Tamr instance"
-  default = 21281
+  default     = 21281
 }
 
 variable "kibana_port" {
-  type = number
+  type        = number
   description = "Default Kibana port"
-  default = 5601
+  default     = 5601
 }
 
 variable "enable_kibana_port" {
-  type = bool
+  type        = bool
   description = "If set to true, opens the kibana port for ingress"
-  default = true
+  default     = true
 }
 
 variable "grafana_port" {
-  type = number
+  type        = number
   description = "Default Grafana port"
-  default = 31101
+  default     = 31101
 }
 
 variable "enable_grafana_port" {
-  type = bool
+  type        = bool
   description = "If set to true, opens the grafana port for ingress"
-  default = true
+  default     = true
 }
 
 variable "enable_ssh" {
-  type = bool
+  type        = bool
   description = "If set to true, enables SSH"
-  default = true
+  default     = true
 }
 
 variable "enable_ping" {
-  type = bool
+  type        = bool
   description = "If set to true, enables ping"
-  default = true
+  default     = true
 }
 
 variable "ingress_cidr_blocks" {
-  type = list(string)
+  type        = list(string)
   description = "CIDR blocks to attach to security groups for ingress"
-  default = []
+  default     = []
 }
 
 variable "ingress_security_groups" {
-  type = list(string)
+  type        = list(string)
   description = "Existing security groups to attch to new security groups for ingress"
-  default = []
+  default     = []
 }
 
 variable "egress_cidr_blocks" {
-  type = list(string)
+  type        = list(string)
   description = "CIDR blocks to attach to security groups for egress"
-  default = ["0.0.0.0/0"]
+  default     = ["0.0.0.0/0"]
 }
 
 variable "egress_security_groups" {
-  type = list(string)
+  type        = list(string)
   description = "Existing security groups to attch to new security groups for egress"
-  default = []
+  default     = []
 }
 
 variable "security_group_tags" {
-  type = map(string)
+  type        = map(string)
   description = "Additional tags to be attached to the security group created"
-  default = {Author :"Tamr"}
+  default     = { Author : "Tamr" }
 }
