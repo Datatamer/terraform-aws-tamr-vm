7bc88860ffb7f2db7783db67b5cd8602a0bf4776
daniosim <dani.sim@tamr.com>
2020-10-14 13:20:18 -0400

    Add more outputs to modules.

--- examples/minimal/README.md
+++ examples/minimal/README.md
@@ -18,6 +18,17 @@ No requirements.
 
 ## Outputs
 
-No output.
+| Name | Description |
+|------|-------------|
+| emr\_creator\_policy\_arn | ARN of the EMR creator IAM policy created. |
+| s3\_policy\_arns | List of ARNs of S3 policies attached to Tamr user IAM role |
+| subnet\_id | n/a |
+| tamr\_instance\_id | The ID of the Tamr instance created |
+| tamr\_instance\_ip | Private IP address of the Tamr instance |
+| tamr\_instance\_profile\_id | ID of the IAM instance profile created |
+| tamr\_instance\_role\_name | ID of the Tamr user IAM role created |
+| tamr\_key\_pair\_name | Name of EC2 key pair created for Tamr instance |
+| tamr\_security\_group\_id | ID of the security group created |
+| vpc\_id | n/a |
 
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,34 +1,47 @@
+output "vpc_id" {
+  value = aws_vpc.tamr_vm_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.tamr_vm_subnet.id
+}
+
+output "tamr_key_pair_name" {
+  value       = module.tamr_ec2_key_pair.this_key_pair_key_name
+  description = "Name of EC2 key pair created for Tamr instance"
+}
+
 output "tamr_security_group_id" {
-  value       = module.aws-security-groups.tamr_security_group_id
+  value       = module.tamr-vm.tamr_security_group_id
   description = "ID of the security group created"
 }
 
 output "tamr_instance_ip" {
-  value       = module.tamr_instance.tamr_instance_ip
+  value       = module.tamr-vm.tamr_instance_ip
   description = "Private IP address of the Tamr instance"
 }
 
 output "tamr_instance_id" {
-  value       = module.tamr_instance.ec2_instance_id
+  value       = module.tamr-vm.tamr_instance_id
   description = "The ID of the Tamr instance created"
 }
 
 output "tamr_instance_profile_id" {
-  value       = module.aws-iam-role.tamr_instance_profile_id
+  value       = module.tamr-vm.tamr_instance_profile_id
   description = "ID of the IAM instance profile created"
 }
 
 output "tamr_instance_role_name" {
-  value       = module.aws-iam-role.tamr_instance_role_name
+  value       = module.tamr-vm.tamr_instance_role_name
   description = "ID of the Tamr user IAM role created"
 }
 
 output "emr_creator_policy_arn" {
-  value       = module.aws-iam-policies.emr_creator_policy_arn
+  value       = module.tamr-vm.emr_creator_policy_arn
   description = "ARN of the EMR creator IAM policy created."
 }
 
 output "s3_policy_arns" {
-  value       = module.aws-iam-policies.s3_policy_arns
+  value       = module.tamr-vm.s3_policy_arns
   description = "List of ARNs of S3 policies attached to Tamr user IAM role"
 }
