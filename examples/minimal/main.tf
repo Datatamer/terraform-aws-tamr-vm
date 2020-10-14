03ca123fd8e20244499393f1299e4bfa232cdf52
daniosim <dani.sim@tamr.com>
2020-10-14 13:18:10 -0400

    DEV-14424 Remove aws_emrfs_user_policy_name.

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,15 @@
 locals {
   az = "us-east-1e"
+  tamr_vm_s3_actions = [
+    "s3:PutObject",
+    "s3:GetObject",
+    "s3:DeleteObject",
+    "s3:AbortMultipartUpload",
+    "s3:ListBucket",
+    "s3:ListObjects",
+    "s3:CreateJob",
+    "s3:HeadBucket"
+  ]
 }
 
 # Set up VPC & subnet
@@ -15,47 +25,26 @@ resource "aws_subnet" "tamr_vm_subnet" {
 
 # Set up HBase logs bucket
 module "hbase-logs-bucket" {
-  source      = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
-  bucket_name = "test-hbase-logs-bucket"
-  read_write_actions = [
-    "s3:PutObject",
-    "s3:GetObject",
-    "s3:DeleteObject",
-    "s3:AbortMultipartUpload",
-    "s3:ListBucket",
-    "s3:ListObjects"
-  ]
-  read_write_paths = [""] # r/w policy permitting specified rw actions on entire bucket
+  source             = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
+  bucket_name        = "test-hbase-logs-bucket"
+  read_write_actions = local.tamr_vm_s3_actions
+  read_write_paths   = [""] # r/w policy permitting specified rw actions on entire bucket
 }
 
 # Set up HBase root directory bucket
 module "hbase-rootdir-bucket" {
-  source      = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
-  bucket_name = "test-hbase-root-directory-bucket"
-  read_write_actions = [
-    "s3:PutObject",
-    "s3:GetObject",
-    "s3:DeleteObject",
-    "s3:AbortMultipartUpload",
-    "s3:ListBucket",
-    "s3:ListObjects"
-  ]
-  read_write_paths = [""] # r/w policy permitting default rw actions on entire bucket
+  source             = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
+  bucket_name        = "test-hbase-root-directory-bucket"
+  read_write_actions = local.tamr_vm_s3_actions
+  read_write_paths   = [""] # r/w policy permitting default rw actions on entire bucket
 }
 
 # Set up Spark logs bucket
 module "spark-logs-bucket" {
-  source      = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
-  bucket_name = "test-spark-logs-bucket"
-  read_write_actions = [
-    "s3:PutObject",
-    "s3:GetObject",
-    "s3:DeleteObject",
-    "s3:AbortMultipartUpload",
-    "s3:ListBucket",
-    "s3:ListObjects"
-  ]
-  read_write_paths = [""] # r/w policy permitting specified rw actions on entire bucket
+  source             = "git::git@github.com:Datatamer/terraform-aws-s3.git?ref=0.1.0"
+  bucket_name        = "test-spark-logs-bucket"
+  read_write_actions = local.tamr_vm_s3_actions
+  read_write_paths   = [""] # r/w policy permitting specified rw actions on entire bucket
 }
 
 # Create new EC2 key pair
@@ -70,12 +59,11 @@ module "tamr_ec2_key_pair" {
 }
 
 module "tamr-vm" {
-  # source                           = "git::git@github.com:Datatamer/terraform-aws-tamr-vm.git?ref=0.3.0"
+  # source                           = "git::git@github.com:Datatamer/terraform-aws-tamr-vm.git?ref=0.3.2"
   source                           = "../.."
   aws_role_name                    = "test-tamr-ec2-role"
   aws_instance_profile_name        = "test-tamr-ec2-instance-profile"
   aws_emr_creator_policy_name      = "TestEmrCreatorPolicy"
-  aws_emrfs_user_policy_name       = "TestEmrfsS3Policy"
   aws_emrfs_hbase_bucket_name      = module.hbase-rootdir-bucket.bucket_name
   aws_emrfs_hbase_logs_bucket_name = module.hbase-logs-bucket.bucket_name
   aws_emrfs_spark_logs_bucket_name = module.spark-logs-bucket.bucket_name
