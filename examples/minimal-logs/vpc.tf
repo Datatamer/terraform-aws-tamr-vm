17cca63949d3212ee2213028ceacfe42d6e4f2e4
Federico Vazquez <federico.vazquez@caylent.com>
2021-12-16 17:52:59 -0300

    Removes commented line.

--- examples/minimal-logs/vpc.tf
+++ examples/minimal-logs/vpc.tf
@@ -1,7 +1,6 @@
 # Set up VPC & subnet
 module "vpc" {
   source = "git::https://github.com/Datatamer/terraform-aws-networking.git?ref=0.1.0"
-  #ingress_cidr_blocks           = ["172.16.0.0/16"]
   vpc_cidr_block                = "10.0.0.0/16"
   data_subnet_cidr_blocks       = ["10.0.2.0/24", "10.0.3.0/24"]
   application_subnet_cidr_block = "10.0.0.0/24"
