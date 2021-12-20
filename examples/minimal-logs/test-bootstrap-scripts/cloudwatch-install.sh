df97a7948b3e6abc70dc7ecf21f6545b535c5419
Federico Vazquez <federico.vazquez@caylent.com>
2021-12-20 19:18:09 -0300

    Modifies shell script.

--- examples/minimal-logs/test-bootstrap-scripts/cloudwatch-install.sh
+++ examples/minimal-logs/test-bootstrap-scripts/cloudwatch-install.sh
@@ -2,20 +2,7 @@
 os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 case $os in
-        "\"Amazon Linux\"")
-              echo "********** Downloading Cloudwatch Agent **********"
-              curl https://s3.${region}.amazonaws.com/amazoncloudwatch-agent-${region}/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm --output agent.rpm
-              if [[ $(ls -la | grep agent) ]];
-              then
-              echo "Cloudwatch agent installation file was found."
-              else
-              echo "Could not download the Cloudwatch agent installation file."
-              exit 1
-              fi
-              echo "********** Installing Cloudwatch Agent **********"
-              yum install -y ./agent.rpm ;;
-        "\"Red Hat Enterprise Linux\"")
-              pwd
+        "\"Amazon Linux\"" | "\"Red Hat Enterprise Linux\"")
               echo "********** Downloading Cloudwatch Agent **********"
               curl https://s3.${region}.amazonaws.com/amazoncloudwatch-agent-${region}/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm --output agent.rpm
               if [[ $(ls -la | grep agent) ]];
@@ -54,49 +41,49 @@ echo " {
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/unify-all.error.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/unify-all.out.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/unify-events.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/unify.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/auth.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify-auth\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/dataset.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify-dataset\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/recipe.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify-recipe\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      },
                      {
                          \"file_path\": \"/home/ubuntu/tamr/logs/dedup.log\",
                          \"log_group_name\": \"${log_group}\",
-                         \"log_stream_name\": \"${log_stream}\",
+                         \"log_stream_name\": \"unify-dedup\",
                          \"timestamp_format\": \"%H: %M: %S%y%b%-d\"
                      }
                  ]
@@ -105,17 +92,15 @@ echo " {
       }
   } " | tee cloudwatch-config.json
 
+  echo "********** Mounting Cloudwatch Agent config file to Cloduwatch Agent **********"
+  /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
+
 case $os in
-       "\"Amazon Linux\"")
-              echo "********** Mounting Cloudwatch Agent config file to Cloduwatch Agent **********"
-              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
-              amazon-cloudwatch-agent-ctl -a start ;;
-       "\"Red Hat Enterprise Linux\"")
-              echo "********** Mounting Cloudwatch Agent config file to Cloduwatch Agent **********"
-              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
+       "\"Amazon Linux\"" | "\"Red Hat Enterprise Linux\"")
+         echo "********** Starting Cloudwatch Agent **********"
               amazon-cloudwatch-agent-ctl -a start ;;
         "\"Ubuntu\"")
-              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
+        echo "********** Starting Cloudwatch Agent **********"
               systemctl start amazon-cloudwatch-agent.service ;;
         *)    echo "Not recognized OS, aborting..."
 
