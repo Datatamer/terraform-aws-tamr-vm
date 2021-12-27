dee90a5cd4d53db3057444ed5b084a08439a80f9
Federico Vazquez <federico.vazquez@caylent.com>
2021-12-27 17:32:50 -0300

    Fixes variable.

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  effective_policy_arns = length(var.s3_policy_arns) > 0 ? var.s3_policy_arns : var.additional_policy_arns
+  effective_policy_arns = length(var.additional_policy_arns) > 0 ? var.additional_policy_arns : var.s3_policy_arns
 }
 
 //working example for Tamr EMR account
